7952f67f449a47f4494ce113a607ea4088414249
Phan Van Ven <contact.phanvanven@gmail.com>
2024-05-08 19:29:24 +0700

    Add: RegisterDto, Migrations. Update: ApplicationDBContext. Setup: Program

---
+++ Migrations/20240508102010_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1f6486f8-fb9b-43fc-94f6-668fd28a89c0", null, "Admin", "ADMIN" },
+                    { "288b280d-1b74-4f07-9be6-14a48a650443", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1f6486f8-fb9b-43fc-94f6-668fd28a89c0");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "288b280d-1b74-4f07-9be6-14a48a650443");
+        }
+    }
+}
